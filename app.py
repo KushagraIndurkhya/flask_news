388392516926a2d8dcfc815f68e6303ca0c41ffc
KushagraIndurkhya <53645587+KushagraIndurkhya@users.noreply.github.com>
2020-07-06 02:16:31 +0530

    date update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
 from datetime import date
 
-today = date.today()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,6 +17,7 @@ def news_scrape():
     final_news=[]
     for news in news_list:
             final_news.append(news.title.text)
+    today = date.today()
     return render_template("index.html",date=today,news=final_news)
 
 
