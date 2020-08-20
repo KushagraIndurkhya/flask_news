833c7d1fdb8aae0f5dacfdee9db1fdcbafd54cbc
kushagraindurkhya <kushagraindurkhya7@gmail.com>
2020-08-20 19:00:15 +0530

    added links

--- app.py
+++ app.py
@@ -15,10 +15,14 @@ def news_scrape():
     soup_page = soup(xml_page, "lxml")
     news_list = soup_page.findAll("item")
     final_news=[]
+    final_link = []
     for news in news_list:
-            final_news.append(news.title.text)
+        final_news.append(news.title.text)
+        for i, x in enumerate(news.children):
+            if (i == 2):
+                final_link.append(x)
     today = date.today()
-    return render_template("index.html",date=today,news=final_news)
+    return render_template("index.html",date=today,news=final_news,links=final_link)
 
 
 if __name__ == '__main__':
